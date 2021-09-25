cad88ae424a071e16e3ad64e6d5729b838aa3935
İsmail <ismailgok33@gmail.com>
2021-09-26 02:28:05 +0300

    Countdown animations is created

--- TercihKilavuzu2022/Controller/CountdownController.swift
+++ TercihKilavuzu2022/Controller/CountdownController.swift
@@ -11,6 +11,15 @@ class CountdownController: UIViewController {
     
     // MARK: - Properties
     
+    let timeLeftShapeLayer = CAShapeLayer()
+    let bgShapeLayer = CAShapeLayer()
+    var timeLeft: TimeInterval = 250
+    var endTime: TimeInterval = 365
+    var timeLabel =  UILabel()
+    var timer = Timer()
+    // here you create your basic animation object to animate the strokeEnd
+    let strokeIt = CABasicAnimation(keyPath: "strokeEnd")
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -22,6 +31,69 @@ class CountdownController: UIViewController {
     // MARK: Helpers
     
     func configureUI() {
-        view.backgroundColor = .systemPink
+        //view.backgroundColor = .systemPink
+        
+        view.backgroundColor = UIColor(white: 0.94, alpha: 1.0)
+        drawBgShape()
+        drawTimeLeftShape()
+        addTimeLabel()
+        // here you define the fromValue, toValue and duration of your animation
+        strokeIt.fromValue = Float(365-250)/Float(365)
+        strokeIt.toValue = 1
+//        strokeIt.duration = timeLeft
+        strokeIt.duration = (365-250)*60
+        // add the animation to your timeLeftShapeLayer
+        timeLeftShapeLayer.add(strokeIt, forKey: nil)
+        // define the future end time by adding the timeLeft to now Date()
+        //endTime = Date().addingTimeInterval(timeLeft)
+        timer = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+    }
+    
+    func drawBgShape() {
+        bgShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
+            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        bgShapeLayer.strokeColor = UIColor.white.cgColor
+        bgShapeLayer.fillColor = UIColor.clear.cgColor
+        bgShapeLayer.lineWidth = 15
+        view.layer.addSublayer(bgShapeLayer)
+    }
+    func drawTimeLeftShape() {
+        timeLeftShapeLayer.path = UIBezierPath(arcCenter: CGPoint(x: view.frame.midX , y: view.frame.midY), radius:
+            100, startAngle: -90.degreesToRadians, endAngle: 270.degreesToRadians, clockwise: true).cgPath
+        timeLeftShapeLayer.strokeColor = UIColor.myCustomBlue.cgColor
+        timeLeftShapeLayer.fillColor = UIColor.clear.cgColor
+        timeLeftShapeLayer.lineWidth = 15
+        view.layer.addSublayer(timeLeftShapeLayer)
+    }
+    
+    func addTimeLabel() {
+        timeLabel = UILabel(frame: CGRect(x: view.frame.midX-50 ,y: view.frame.midY-25, width: 100, height: 50))
+        timeLabel.textAlignment = .center
+        timeLabel.text = timeLeft.time
+//        timeLabel.text = endTime.time
+        view.addSubview(timeLabel)
+    }
+    
+    @objc func updateTime() {
+        if timeLeft > 0 {
+            timeLeft -= 1
+            timeLabel.text = timeLeft.time
+        } else {
+            timeLabel.text = "00:00"
+            timer.invalidate()
+        }
+    }
+}
+
+
+extension TimeInterval {
+    var time: String {
+        return String(format:"%02d:%02d", Int(self/365),  Int(ceil(truncatingRemainder(dividingBy: 365))) )
+    }
+}
+extension Int {
+    var degreesToRadians : CGFloat {
+        return CGFloat(self) * .pi / 180
     }
 }
